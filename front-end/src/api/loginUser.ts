f1055bfe031cf8dc546200f0fc73da13f41c45da
Marcos Albuquerque <mpdsa@pm.me>
2023-01-20 19:42:13 -0300

    Mock request to authenticate user and return token

---
+++ front-end/src/api/loginUser.ts
@@ -0,0 +1,10 @@
+export default function login(username: string, pass: string) {
+  const user = 'desafiosharenergy'
+  const password = 'sh@r3n3rgy'
+
+  if(username === user && pass === password) {
+    return 'sadalskndsakldnasklndedno12n3o13n12io981239812h89'
+  } 
+  
+  return false
+}
