1faa4ba23b65034b3d9b917e910b21783418baff
Marcos Albuquerque <mpdsa@pm.me>
2023-01-20 20:16:59 -0300

    Display random dog photo, display preload

---
+++ front-end/src/component/DashboardWindowDog.tsx
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { Suspense, useState } from 'react';
+import getDogRandom from '../api/dogRandomImage';
+
+export default function DashboardWindowDog() {
+  const [dog, setDog] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <div className='w-full flex flex-col items-center justify-center mt-10 gap-10'>
+      <button
+        className='border w-1/3 p-4 bg-green-500 hover:bg-green-400 shadow-xl'
+        type='submit'
+        onClick={async () => {
+          setLoading(true);
+          const test = await getDogRandom();
+          setDog(test);
+          setLoading(false);
+        }}
+      >
+        Gerar nova imagem
+      </button>
+
+      {loading ? <Loading /> : <img className='w-1/2' src={dog} />}
+    </div>
+  );
+}
+
+const Loading = () => {
+  return (
+    <div>
+      <p>Carregando...</p>
+    </div>
+  );
+};
